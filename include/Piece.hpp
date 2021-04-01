fb7fb3860f771b1eef5cceac5817a7f1ab4a8721
CORIAT BASTIEN <bastien.coriat@etu.unistra.fr>
2021-04-02 01:38:57 +0200

    minor changes

--- include/Piece.hpp
+++ include/Piece.hpp
@@ -32,13 +32,15 @@ public:
 	//et un vecteur destination
     SDL_Rect src[4];
 	SDL_Rect dst[4];
-	SDL_Rect locTetris;
+	static SDL_Rect locTetris;
 	int color[3];
 	int pastel[3];
 
 	//il faudrait aussi quune pièce ait une couleur.
 public :
     Piece(SDL_Rect locTetris);
+	void initStaticMembers(SDL_Rect sizeTetris);
+
     virtual ~Piece(); // destructor
 
 	virtual void update();
@@ -58,15 +60,88 @@ public :
 	bool right(bool moveSource=true);
 	bool left(bool moveSource=true);
 	void rotateLeft(bool moveSource=true);
+	
 	virtual void rotateRight(bool moveSource=true);
 
+	/**
+	 * @brief This function check the position of the piece temp in the matrice 
+	 * mat. It doesnt modify this.
+	 * @param temp is the piece to check legality
+	 * @param mat is the matrice of tetris. mat[i][j] is true if a piece 
+	 * is present, false either.
+	 * @return an instance of class Error. Error has the following attributes : 
+	 * COLLISION_PIECE is true if a piece is already present in the same location 
+	 * of temp.
+	 * OVER_X is true if a rectangle from piece exceeds the dimension of mat
+	 * along x axis.
+	 * OVER_Y is true if a rectangle from piece the dimension of mat
+	 * along y axis.
+	 * OVER_NUMBER_X is an integer. it measures how much the piece exceeds
+	 * the x dimension.
+	 * NO_ERROR if temp is in a legal place. 
+	 * 
+	 */
 	Error isLegalPosition(Piece *temp, bool mat[BLOCSX][BLOCSY]);
+	
+	/**
+	 * @brief This function check if a piece can be translated. It 
+	 *	creates a copy of 'this', translate it and callls isLegalPostion.   
+	 * @param a : translation of a along x axis
+	 * @param b : translation of b along y axis
+	 * @param mat : the matrice of the tetris game. 
+	 * @return an instance of class Error (cf isLegalPostition for 
+	 * more explanations)
+	 */
 	Error isLegalTranslate(int a, int b, bool mat[BLOCSX][BLOCSY]);
+	
+	/**
+	 * @brief This function calls isLegalTranslate (1,0,mat). 
+	 * Cf isLegalTranslate.
+	 * @param mat
+	 * @return isLegalTranslate (1,0,mat)
+	 */
 	Error isLegalRight(bool mat[BLOCSX][BLOCSY]);
+	
+	/**
+	 * @brief This function calls isLegalTranslate (-1,0,mat). 
+	 * Cf isLegalTranslate.
+	 * @param mat
+	 * @return isLegalTranslate (-1,0,mat)
+	 */
 	Error isLegalLeft(bool mat[BLOCSX][BLOCSY]);
+	
+	/**
+	 * @brief This function calls isLegalTranslate (0,1,mat). 
+	 * Cf isLegalTranslate.
+	 * @param mat
+	 * @return isLegalTranslate (0,1,mat)
+	 */
 	Error isLegalDown(bool mat[BLOCSX][BLOCSY]);
+	
+	/**
+	 * @brief This function calls isLegalTranslate (0,-1,mat). 
+	 * Cf isLegalTranslate.
+	 * @param mat
+	 * @return isLegalTranslate (0,-1,mat)
+	 */
 	Error isLegalUp(bool mat[BLOCSX][BLOCSY]);
+	
+	/**
+	 * @brief This function makes a copy of 'this', rotates is and calls 
+	 * isLegalPostion on the copy. So 'this' is not modify.
+	 * @param mat
+	 * @return an instance of class Error. Cf isLegalPosition for more 
+	 * explanations.
+	 */
 	Error isLegalRotateLeft(bool mat[BLOCSX][BLOCSY]);
+	
+	/**
+	 * @brief This function makes a copy of 'this', rotates is and calls 
+	 * isLegalPostion on the copy. So 'this' is not modify.
+	 * @param mat
+	 * @return an instance of class Error. Cf isLegalPosition for more 
+	 * explanations.
+	 */
 	virtual Error isLegalRotateRight(bool mat[BLOCSX][BLOCSY]);
 
 	bool onDown(bool mat[BLOCSX][BLOCSY], bool cont, SDL_Renderer* renderer,SDL_Texture* blank,
@@ -80,13 +155,15 @@ public :
 	void affiche_coord(bool source, bool dest);
 
 	void cheat(bool mat[BLOCSX][BLOCSY]);
+	void holdPiece(bool mat[BLOCSX][BLOCSY]);
+	int nbFullLine(bool mat[BLOCSX][BLOCSY]);
 
 };
 
 class JTetri : public Piece {
 	public:
 		JTetri(SDL_Rect sizeTetris);
-		void update();
+		void update() override;
 };
 
 class LTetri : public Piece {
@@ -98,7 +175,7 @@ class LTetri : public Piece {
 class ITetri : public Piece {
 	public:
 		ITetri(SDL_Rect sizeTetris);
-		void update();
+		void update() override;
 };
 
 
